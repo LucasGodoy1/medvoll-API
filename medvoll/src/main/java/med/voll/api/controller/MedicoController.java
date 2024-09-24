2a63b9768385a7ff1e3d1a74e33952907a8f720b
Lucas Godoy <lucassdegodoy@hotmail.com>
2024-09-23 21:04:44 -0300

    feat: ordenação

--- medvoll/src/main/java/med/voll/api/controller/MedicoController.java
+++ medvoll/src/main/java/med/voll/api/controller/MedicoController.java
@@ -8,6 +8,7 @@ import med.voll.api.entity.Medico;
 import med.voll.api.service.MedicoService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -25,10 +26,10 @@ public class MedicoController {
     public ResponseEntity<DadosCadastroMedicoDTO> cadastrarMedico(@RequestBody  @Valid DadosCadastroMedicoDTO dadosCadastroMedicoDTO) {
         medicoService.salvarMedico(dadosCadastroMedicoDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(dadosCadastroMedicoDTO);
-    }
+    } //localhost:8080/medicos/listarMedicos?size=5&page=2&sort=nome,desc
 
     @GetMapping("listarMedicos")
-    ResponseEntity<Page<ListarMedicoDTO>> listarMedicos(Pageable pageable) {
+    ResponseEntity<Page<ListarMedicoDTO>> listarMedicos(@PageableDefault(size = 10, sort = {"nome"}) Pageable pageable) {
 
         return ResponseEntity.ok(medicoService.listarMedicos(pageable));
     }
--- medvoll/src/main/java/med/voll/api/service/MedicoService.java
+++ medvoll/src/main/java/med/voll/api/service/MedicoService.java
@@ -7,6 +7,7 @@ import med.voll.api.entity.Medico;
 import med.voll.api.repository.MedicoRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
